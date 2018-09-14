5ec924ebdcdf7dcd31d1d292f5e9873d56d3b3d0
Ritvikjain <ritvikjain2@gmail.com>
2018-09-14 22:00:13 +0530

    about updated

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -45,4 +45,10 @@ export class AboutPage {
     this.photourl = auth.user.photoURL;
   }
 
+  logout(){
+    this.auth.signOut().then(item => {
+      this.app.getRootNav().setRoot(LoginPage);
+    });
+  }
+
 }
